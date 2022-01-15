324d53df5b5eb6251bd60f7c0cd3db783dad0305
cjcanlas01 <cjcanlas78@gmail.com>
2022-01-16 01:15:30 +0800

    add config source checker

---
+++ src/config.js
@@ -0,0 +1,52 @@
+const { access } = require("fs/promises");
+const { constants } = require("fs");
+const axios = require("axios");
+const { GIST_ID, GIST_FILENAME } = require("./env-config");
+
+/**
+ * Get Github Gist URL
+ *
+ * @param {string} gistId
+ * @returns {string}
+ */
+const gistUrl = (gistId) => `https://api.github.com/gists/${gistId}`;
+
+/**
+ * Check if local config file exists inside project
+ *
+ * @returns {boolean}
+ */
+const checkIfConfigFileExists = async () => {
+  try {
+    await access("./config.json", constants.R_OK | constants.W_OK);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+/**
+ * Get config content based on whether file exists inside project
+ *
+ * @returns {object}
+ */
+const getConfig = async () => {
+  if (checkIfConfigFileExists()) {
+    const gist = await axios.get(gistUrl(GIST_ID));
+    const parsedConfig = JSON.parse(gist.data.files[GIST_FILENAME].content);
+    return {
+      source: "remote",
+      config: parsedConfig,
+    };
+  }
+
+  return {
+    source: "local",
+    config: require("../config.json"),
+  };
+};
+
+module.exports = {
+  checkIfConfigFileExists,
+  getConfig,
+};
--- src/env-config.js
+++ src/env-config.js
@@ -4,4 +4,6 @@ module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN,
   CLIENT_ID: process.env.CLIENT_ID,
   DATABASE_URL: process.env.DATABASE_URL,
+  GIST_ID: process.env.GIST_ID,
+  GIST_FILENAME: process.env.GIST_FILENAME,
 };
