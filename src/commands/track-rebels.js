96d905d363035f84e6c5d530bf201d2c17af2d1d
cjcanlas01 <cjcanlas78@gmail.com>
2022-05-29 23:20:31 +0800

    Update track-rebel

--- src/commands/track-rebels.js
+++ src/commands/track-rebels.js
@@ -6,7 +6,7 @@ const path = require("path");
 const { initializeSheet } = require("../utils/google-spreadsheet");
 const { getConfigs } = require("../config");
 const { postSelf, checkIfUserIsAllowed } = require("../utils/utils");
-const FTP = require('ftp')
+const FTP = require("ftp");
 const c = new FTP();
 
 const INTERACTION_MESSAGE = {
@@ -28,24 +28,63 @@ const getRebelsCSV = (path) => {
   return fs.readFileSync(csvPath, "utf8");
 };
 
-const getRebelsCSV_FTP = () => {
+const getRebelsCSV_FTP = (path) => {
   const csvPath = path.resolve(__dirname, "../../rebels.csv");
   c.connect({
     host: "mad.psykoral.com",
     user: "madgotwic",
-    password: "Ai7USLJ3"
+    password: "Ai7USLJ3",
   });
 
-  c.on('ready', function() {
-    c.get('/mad.psykoral.com/rebels.csv', function(err, stream) {
+  c.on("ready", function () {
+    c.get("/mad.psykoral.com/rebels.csv", function (err, stream) {
       if (err) throw err;
-      stream.once('close', function() { ftp_client.end(); });
+      stream.once("close", function () {
+        c.end();
+      });
       stream.pipe(fs.createWriteStream(csvPath));
     });
   });
 
   return fs.readFileSync(csvPath, "utf8");
-}
+};
+
+const updateWeeklyContributionCells = async (sheet, column, data) => {
+  await sheet.loadCells();
+  for (let r = 0; r < 100; r++) {
+    let row = r + 2;
+    for (let c = 0; c < 3; c++) {
+      let col = column + c;
+      const cell = sheet.getCell(row, col);
+      cell.value = "";
+      if (data.length > r) {
+        cell.value = data[r][c];
+      }
+    }
+  }
+  await sheet.saveUpdatedCells();
+};
+
+const updateMemberDataCells = async (sheet, data) => {
+  await sheet.loadCells();
+  const temp = [...data];
+  temp.sort((a, b) => {
+    if (a[0] == b[0]) return 0;
+    else return a[0] < b[0] ? -1 : 1;
+  });
+  for (let r = 0; r < 100; r++) {
+    let row = r + 1;
+    for (let col = 0; col < 2; col++) {
+      const cell = sheet.getCell(row, col);
+      cell.value = "";
+      if (temp.length > r) {
+        cell.value = temp[r][col];
+      }
+    }
+  }
+
+  await sheet.saveUpdatedCells();
+};
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -54,7 +93,7 @@ module.exports = {
   async execute(interaction) {
     const action = new Interaction(interaction);
     const { TRACK_REBELS } = await getConfigs();
-    const rebelsList = getRebelsCSV_FTP();
+    const rebelsList = getRebelsCSV_FTP(path);
     const isUserAllowed = await checkIfUserIsAllowed(action);
 
     if (!isUserAllowed) {
@@ -71,17 +110,16 @@ module.exports = {
     }
 
     await interaction.reply(INTERACTION_MESSAGE.START_PROCESS);
-    const sheetContents = parseRebelsList(rebelsList);
+    const rebelData = parseRebelsList(rebelsList);
     const sheet = await initializeSheet();
-    const weeklyData = sheet.sheetsByTitle[TRACK_REBELS.REBEL_DATA]
-    const secondRow = await weeklyData.getRows({offset: 1})
-    console.log(secondRow[0].length)
-    console.log(rows[0])
-    // const rlSheet = sheet.sheetsByTitle[TRACK_REBELS.SHEET];
-    // await rlSheet.clear(TRACK_REBELS.RANGE);
-    // Remove header as already existing in the sheet
-    sheetContents.shift();
-    await rlSheet.addRows(sheetContents);
+    const weeklyData = sheet.sheetsByTitle[TRACK_REBELS.REBEL_DATA];
+    const memberData = sheet.sheetsByTitle[TRACK_REBELS.MEMBER_DATA];
+    const secondRow = await weeklyData.getRows({ offset: 0, limit: 1 });
+    const rangeStart = secondRow[0]._rawData.length - TRACK_REBELS.WEEK_RANGE;
+
+    rebelData.shift();
+    await updateWeeklyContributionCells(weeklyData, rangeStart, rebelData);
+    await updateMemberDataCells(memberData, rebelData);
     await interaction.editReply({
       content: INTERACTION_MESSAGE.END_PROCESS,
     });
